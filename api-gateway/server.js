9542e86f3cb61fe6c0f29a4243671ea67e015123
Jacob <jgoldverg@gmail.com>
2019-10-22 00:49:31 -0400

    api gate way working now need to start making routes and logic

--- api-gateway/server.js
+++ api-gateway/server.js
@@ -1,10 +1,13 @@
-const app = require('express')
-const app = require('body-parser')
+
+// server.js
+
+const express = require("express");
+const app = express();
 
 app.get("/", (req, res) => {
-    res.send("Hello from Node.js app \n");
+  res.send("Hello from Node.js app \n");
 });
 
-app.listen('7080', () => {
-    console.log("Api-gateway sucks!")
+app.listen('7080', () =>{
+  console.log("Mongo Sucks!")
 })
